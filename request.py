33cb6406ce9ca4907f81f4e0080f9ff3cffbb11c
jdidi-boujneh <52798616+jdidi-boujneh@users.noreply.github.com>
2020-09-18 15:41:08 +0200

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'aa':0.5, 'bb':0.5, 'cc':1,'a':1, 'b':1, 'c':0.5,'d':0.5, 'e':0.5, 'f':0.5})
 
 print(r.json())
